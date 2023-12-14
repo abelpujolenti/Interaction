b36e08a58dacf7aba2a74b01cf52510d15de63df
abel pujol <abel.pujol@enti.cat>
2023-12-14 09:26:34 +0100

    Project Start

---
+++ MyUnityProject/Assets/Scripts/MyOctopusController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+
+namespace OctopusController
+{
+    public enum TentacleMode { LEG, TAIL, TENTACLE };
+
+    public class MyOctopusController
+    {
+
+        MyTentacleController[] _tentacles = new MyTentacleController[4];
+
+        Transform _currentRegion;
+        Transform _target;
+
+        Transform[] _randomTargets;// = new Transform[4];
+        Vector3[] _targetOffsets;
+
+        float _twistMin, _twistMax;
+        float _swingMin, _swingMax;
+
+        #region public methods
+        //DO NOT CHANGE THE PUBLIC METHODS!!
+
+        public float TwistMin { set => _twistMin = value; }
+        public float TwistMax { set => _twistMax = value; }
+        public float SwingMin { set => _swingMin = value; }
+        public float SwingMax { set => _swingMax = value; }
+
+
+        public void TestLogging(string objectName)
+        {
+
+
+            Debug.Log("hello, I am initializing my Octopus Controller in object " + objectName);
+
+
+        }
+
+        public void Init(Transform[] tentacleRoots, Transform[] randomTargets)
+        {
+            Transform[] actualTentacleRoots = new Transform[tentacleRoots.Length];
+            for (int i = 0; i < tentacleRoots.Length; i++)
+            {
+                actualTentacleRoots[i] = tentacleRoots[i].GetChild(0).GetChild(0);
+            }
+            tentacleRoots = actualTentacleRoots;
+            _tentacles = new MyTentacleController[tentacleRoots.Length];
+            _targetOffsets = new Vector3[tentacleRoots.Length];
+
+            _randomTargets = randomTargets;
+
+            // foreach (Transform t in tentacleRoots)
+            for (int i = 0; i < tentacleRoots.Length; i++)
+            {
+
+                _tentacles[i] = new MyTentacleController();
+                _tentacles[i].LoadTentacleJoints(tentacleRoots[i], TentacleMode.TENTACLE);
+                _targetOffsets[i] = _tentacles[i].Bones[_tentacles[i].Bones.Length - 1].position - randomTargets[i].position;
+                //TODO: initialize any variables needed in ccd
+            }
+            //TODO: use the regions however you need to make sure each tentacle stays in its region
+
+        }
+
+
+        public void NotifyTarget(Transform target, Transform region)
+        {
+            _currentRegion = region;
+            _target = target;
+        }
+
+        public void NotifyShoot()
+        {
+            //TODO. what happens here?
+            Debug.Log("Shoot");
+        }
+
+
+        public void UpdateTentacles()
+        {
+            //TODO: implement logic for the correct tentacle arm to stop the ball and implement CCD method
+            update_ccd();
+        }
+
+        #endregion
+
+
+        #region private and internal methods
+        //todo: add here anything that you need
+
+        void update_ccd()
+        {
+
+            for (int i = 0; i < _tentacles.Length; i++)
+            {
+                for (int t = _tentacles[i].Bones.Length - 2; t >= 0; t--)
+                {
+                    Transform currentBone = _tentacles[i].Bones[t];
+                    Transform tipBone = _tentacles[i].Bones[_tentacles[i].Bones.Length - 1];
+                    Vector3 target = _randomTargets[i].position + _targetOffsets[i];
+
+
+                    Vector3 currentToTip = (tipBone.position - currentBone.position).normalized;
+                    Vector3 currentToTarget = (target - currentBone.position).normalized;
+
+                    Quaternion q = Quaternion.FromToRotation(currentToTip, currentToTarget);
+                    currentBone.rotation = q * currentBone.rotation;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MyUnityProject/Assets/Scripts/MyTentacleController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+
+
+namespace OctopusController
+{    
+    internal class MyTentacleController
+
+    //MAINTAIN THIS CLASS AS INTERNAL
+    {
+
+        TentacleMode tentacleMode;
+        Transform[] _bones;
+        Transform _endEffectorSphere;
+
+        public Transform[] Bones { get => _bones; }
+        public Transform EndEffector => _endEffectorSphere;
+
+        //Exercise 1.
+        public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
+        {
+            
+            //TODO: add here whatever is needed to find the bones forming the tentacle for all modes
+            //you may want to use a list, and then convert it to an array and save it into _bones
+            tentacleMode = mode;
+
+            switch (tentacleMode){
+                case TentacleMode.LEG:
+
+                    LoadJoints(2, root);                    
+                    break;
+                
+                case TentacleMode.TAIL:
+
+                    LoadJoints(4, root);
+                    break;
+                
+                case TentacleMode.TENTACLE:
+                    //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
+                    LoadJoints(50, root, 0);
+                    break;
+            }
+            return Bones;
+        }
+
+        private void LoadJoints(int jointsLength, Transform root, int childIndex = 1)
+        {
+            List<Transform> transformList = new List<Transform> { root };
+            
+            for (int i = 0; i < jointsLength; i++)
+            {
+                transformList.Add(transformList[i].GetChild(childIndex));
+            }
+            _endEffectorSphere = transformList[transformList.Count - 1].GetChild(childIndex);
+                    
+            _bones = transformList.ToArray();
+        }
+    }
+}
